88f0f0c888e9a4dade6e34039a37c3db6e921042
NguyenHoangVu-2004 <vunhph45875@fpt.edu.vn>
2024-03-23 09:34:20 +0700

    no message

--- views/noidungwebsite/noidungwebsite.php
+++ views/noidungwebsite/noidungwebsite.php
@@ -1,280 +1,85 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Dashboard</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
-
-<body id="page-top">
-
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"><img style="width: 80px;height: 80px;margin-bottom: 40px;" src="assets/admin/img/King Clothes-logos_white.png" alt=""></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">King Clothes</div>
-            </a>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Trang chủ</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-
-            <!-- Nav Item - Pages Collapse Menu -->
-
-
-            <!-- Nav Item - Utilities Collapse Menu -->
-
-
-
-            <!-- Nav Item - Pages Collapse Menu -->
-
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <span>Đăng nhập</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <span>Người dùng</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <span>Chức vụ</span></a>
-            </li>
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <span>Danh mục</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <span>Sản phẩm</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <span>Khuyến mãi</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?act=baiviet">
-                    <span>Bài viết</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?act=binhluan">
-                    <span>Bình luận</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?act=danhgia">
-                    <span>Đánh giá</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?act=noidung">
-                    <span>Nội dung website</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?act=lienhe">
-                    <span>Liên hệ</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-            <!-- Sidebar Message -->
-        </ul>
-        <!-- End of Sidebar -->
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
-            <div id="content">
-
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
-                    <!-- Topbar Search -->
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Tìm kiếm..." aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </nav>
+<?php
+include "./views/layout/header.php";
+include "./views/layout/menu.php";
+?>
 
 
 
 
 
+<!-- MAIN CHÍNH (CẦN SỬA) -->
+<div class="container-fluid">
+    <h1>Nội dung website</h1>
+    <style>
+        table {
+            text-align: center;
+        }
 
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
 
-                <!-- MAIN CHÍNH (CẦN SỬA) -->
-                <div class="container-fluid">
-                    <h1>Nội dung website</h1>
-                    <style>
-                        table {
-                            text-align: center;
-                        }
+        th,
+        td {
+            width: 250px;
+        }
 
-                        table,
-                        th,
-                        td {
-                            border: 1px solid black;
-                        }
-                        th,td{
-                            width: 250px;
-                        }
-                        table {
-                            border-collapse: collapse;
-                            margin-bottom: 50px;
-                        }
-                        td input{
-                            background-color: red;
-                            border: none;
-                            color: white;
-                            border-radius: 3px;
-                        }
-                        .button input{
-                            background-color: #4066D4;
-                            border:none;
-                            color: white;
-                            padding: 10px 20px;
-                            border-radius: 3px;
-                        }
-                    </style>
-                    <table align="center">
-                        <tr>
-                            <th>ID nội dung</th>
-                            <th>Tên trang web</th>
-                            <th>Ảnh (Logo,...)</th>
-                            <th>Nội dung </th>
-                            <th>Link liên kết </th>
-                            <th>Hành động </th>
-                        </tr>
-                        <?php
-                        // var_dump($danhgia);
-                        foreach ($noidung as $item) : ?>
-                            <tr>
-                                <td><?php echo $item['id_noi_dung']; ?></td>
-                                <td><?php echo $item['ten_trang_web']; ?></td>
-                                <td>
-                                    <img src="assets/admin/img/<?=$item['anh']?>" width="200" height="200" alt="">
-                                </td>
-                                <td><?php echo $item['noi_dung']; ?></td>
-                                <td><?php echo $item['link_lien_ket']; ?></td>
-                                <td>
-                                    <a href="?act=suanoidung&id="><input type="submit" value="Sửa" ></a>
-                                    <a href="?act=suanoidung&id=" onclick="return confirm('Bạn có chắc chăn muốn xoá nội dung này không?')"><input type="submit" value="Xoá" ></a>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </table>
-                    <div class="button">
-                        <a href="?act=themnoidung"><input type="submit" value="Thêm nội dung" name="them"></a>
-                    </div>
-                </div>
-            </div>
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; King Clothes 2024</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
+        table {
+            border-collapse: collapse;
+            margin-bottom: 50px;
+        }
 
-        </div>
-        <!-- End of Content Wrapper -->
+        td input {
+            background-color: red;
+            border: none;
+            color: white;
+            border-radius: 3px;
+        }
 
+        .button input {
+            background-color: #4066D4;
+            border: none;
+            color: white;
+            padding: 10px 20px;
+            border-radius: 3px;
+        }
+    </style>
+    <table align="center">
+        <tr>
+            <th>ID nội dung</th>
+            <th>Tên trang web</th>
+            <th>Ảnh (Logo,...)</th>
+            <th>Nội dung </th>
+            <th>Link liên kết </th>
+            <th>Hành động </th>
+        </tr>
+        <?php
+        // var_dump($danhgia);
+        foreach ($noidung as $item) : ?>
+            <tr>
+                <td><?php echo $item['id_noi_dung']; ?></td>
+                <td><?php echo $item['ten_trang_web']; ?></td>
+                <td>
+                    <img src="assets/admin/img/<?= $item['anh'] ?>" width="200" height="200" alt="">
+                </td>
+                <td><?php echo $item['noi_dung']; ?></td>
+                <td><?php echo $item['link_lien_ket']; ?></td>
+                <td>
+                    <a href="?act=suanoidung&id="><input type="submit" value="Sửa"></a>
+                    <a href="?act=suanoidung&id=" onclick="return confirm('Bạn có chắc chăn muốn xoá nội dung này không?')"><input type="submit" value="Xoá"></a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    <div class="button">
+        <a href="?act=themnoidung"><input type="submit" value="Thêm nội dung" name="them"></a>
     </div>
-    <!-- End of Page Wrapper -->
+</div>
+</div>
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
 
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="assets/admin/vendor/jquery/jquery.min.js"></script>
-    <script src="assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="././assets/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="assets/admin/js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="assets/admin/vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="assets/admin/js/demo/chart-area-demo.js"></script>
-    <script src="assets/admin/js/demo/chart-pie-demo.js"></script>
-
-</body>
-
-</html>
+<?php
+include "./views/layout/footer.php";
+?>
--- views/noidungwebsite/themnoidung.php
+++ views/noidungwebsite/themnoidung.php
@@ -1,175 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Dashboard</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
-
-<body id="page-top">
-
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"><img style="width: 80px;height: 80px;margin-bottom: 40px;" src="assets/admin/img/King Clothes-logos_white.png" alt=""></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">King Clothes</div>
-            </a>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Trang chủ</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-
-            <!-- Nav Item - Pages Collapse Menu -->
-
-
-            <!-- Nav Item - Utilities Collapse Menu -->
+<?php
+include "./views/layout/header.php";
+include "./views/layout/menu.php";
+?>
 
 
 
-            <!-- Nav Item - Pages Collapse Menu -->
+<!-- MAIN CHÍNH (CẦN SỬA) -->
+<div class="container-fluid">
+    <h1>Thêm nội dung website</h1>
+    <form action="" method="post" enctype="multipart/form-data">
+        <label for="">Tên trang web</label>
+        <input type="text" name="ten_trang_web">
+        <label for="">Ảnh</label>
+        <input type="file" name="anh">
+        <label for="">Nội dung</label>
+        <input type="text" name="noi_dung">
+        <label for="">Link liên kết</label>
+        <input type="link" name="link_lien_ket">
+        <br>
+        <input type="reset">
+        <input type="submit" value="Thêm">
 
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <span>Đăng nhập</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <span>Người dùng</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <span>Chức vụ</span></a>
-            </li>
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <span>Danh mục</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <span>Sản phẩm</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <span>Khuyến mãi</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?act=baiviet">
-                    <span>Bài viết</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?act=binhluan">
-                    <span>Bình luận</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?act=danhgia">
-                    <span>Đánh giá</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?act=noidung">
-                    <span>Nội dung website</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?act=lienhe">
-                    <span>Liên hệ</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-            <!-- Sidebar Message -->
-        </ul>
-        <!-- End of Sidebar -->
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
-            <div id="content">
-
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
-                    <!-- Topbar Search -->
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Tìm kiếm..." aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </nav>
-
-
-
-
-
-
-
-                <!-- MAIN CHÍNH (CẦN SỬA) -->
-                <div class="container-fluid">
-                    <h1>Thêm nội dung website</h1>
-                    <form action="" method="post" enctype="multipart/form-data">
-                        <label for="">Tên trang web</label>
-                        <input type="text" name="ten_trang_web">
-                        <label for="">Ảnh</label>
-                        <input type="file" name="anh">
-                        <label for="">Nội dung</label>
-                        <input type="text" name="noi_dung">
-                        <label for="">Link liên kết</label>
-                        <input type="link" name="link_lien_ket">
-                        <br>
-                        <input type="reset">
-                        <input type="submit" value="Thêm">
-
-                    </form>
-                    <!-- <style>
+    </form>
+    <!-- <style>
                         table {
                             text-align: center;
                         }
@@ -186,7 +39,7 @@
                             border-collapse: collapse;
                         }
                     </style> -->
-                    <!-- <table align="center">
+    <!-- <table align="center">
                         <tr>
                             <th>ID nội dung</th>
                             <th>Tên trang web</th>
@@ -201,72 +54,15 @@
                                 <td><?php echo $item['id_noi_dung']; ?></td>
                                 <td><?php echo $item['ten_trang_web']; ?></td>
                                 <td>
-                                    <img src="assets/admin/img/<?=$item['anh']?>" width="200" height="200" alt="">
+                                    <img src="assets/admin/img/<?= $item['anh'] ?>" width="200" height="200" alt="">
                                 </td>
                                 <td><?php echo $item['noi_dung']; ?></td>
                                 <td><?php echo $item['link_lien_ket']; ?></td>
                             </tr>
                         <?php endforeach; ?>
                     </table> -->
-                </div>
-            </div>
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; King Clothes 2024</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
-
-        </div>
-        <!-- End of Content Wrapper -->
-
-    </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="assets/admin/vendor/jquery/jquery.min.js"></script>
-    <script src="assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="././assets/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="assets/admin/js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="assets/admin/vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="assets/admin/js/demo/chart-area-demo.js"></script>
-    <script src="assets/admin/js/demo/chart-pie-demo.js"></script>
-
-</body>
-
-</html>
+</div>
+</div>
+<?php
+include "./views/layout/footer.php";
+?>
